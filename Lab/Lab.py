63f360c289c533c47e6d4dd9faf8acb2502980bf
Nagibuchka <vladislavkucerenko6@gmail.com>
2025-02-19 19:05:45 +0300

    Додав файл Lab.py

---
+++ Lab/Lab.py
@@ -0,0 +1,13 @@
+import datetime
+
+def greet(name: str) -> str:
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    return f"Hello, {name}! The current time is {current_time}."
+
+def main():
+    user_name = input("Enter your name: ")
+    message = greet(user_name)
+    print(message)
+
+if __name__ == "__main__":
+    main()
